7c5614566393622ce41c9b37702dc08e8c57110e
3hui112 <shinetheworld112@gmail.com>
2022-03-13 23:04:42 +0900

    create Posts file

---
+++ src/main/java/com/jojoldu/webservice/domain/Posts.java
@@ -0,0 +1,29 @@
+package com.jojoldu.webservice.domain;
+
+import lombok.Builder;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Posts {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/jojoldu/webservice/domain/PostsRepository.java
@@ -0,0 +1,6 @@
+import com.jojoldu.webservice.domain.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
